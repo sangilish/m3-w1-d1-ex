f955d6f60a517e0b55415cf36b1a56af3bbb1184
sangilish <56685007+sangilish@users.noreply.github.com>
2024-09-03 20:18:33 -0700

    test1

---
+++ node-example3.js
@@ -0,0 +1,16 @@
+const rect = {
+    perimeter: (x, y) => 2 * (x + y),
+    area: (x, y) => x * y
+};
+
+
+function solveRect(l, w) {
+    console.log(`Solving for rectangle with dimensions: ${l}, ${w}`); //Lebgth and width
+
+    if (l <= 0 || w <= 0) { //if  statement for l or w is less than 0
+        console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
+    } else {
+        console.log(`The area of the rectangle is ${rect.area(l, w)}`);
+        console.log(`The perimeter of the rectangle is ${rect.perimeter(l, w)}`);
+    }
+}
